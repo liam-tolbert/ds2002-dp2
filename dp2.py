7ef36fbb459b52c86a305d3baa962731f25ca053
Liam Tolbert <treblotmail@gmail.com>
2024-05-02 05:55:10 +0000

    dp2 done

--- dp2.py
+++ dp2.py
@@ -3,8 +3,14 @@ from bson.json_util import dumps
 import os
 import json
 from dotenv import load_dotenv
+x = 0
+y = 0
+z = 0
 
 def load_json_into_collection(filename):
+    global x;
+    global y;
+    global z;
     # assuming you have defined a connection to your db and collection already:
     f = open(f"./data/{filename}")
 
@@ -13,6 +19,8 @@ def load_json_into_collection(filename):
       file_data = json.load(f)
     except Exception as e:
       print(e, "error when loading", f)
+      x += 1
+      return
     # Inserting the loaded data in the collection
     # if JSON contains data more than one entry
     # insert_many is used else insert_one is used
@@ -21,11 +29,16 @@ def load_json_into_collection(filename):
         collection.insert_many(file_data)
       except Exception as e:
         print(e, "when importing into Mongo")
+        y += 1
+        return
     else:
       try:
         collection.insert_one(file_data)
       except Exception as e:
         print(e)
+        y += 1
+        return
+    z += 1
 
 load_dotenv()
 MONGOPASS = os.getenv('MONGOPASS')
@@ -38,6 +51,8 @@ collection = db.data
 
 files = os.listdir("./data")
 for f in files:
-    print(f)
     load_json_into_collection(f)
 
+print(f"Successfully imported {z} complete documents")
+print(f"{y} complete documents could not be imported")
+print(f"{x} corrupted documents exist in the fileset")
